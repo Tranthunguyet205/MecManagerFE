dcb74f6d498cd94289ac85d388bc2d3d0ff12275
Tranthunguyet205 <tranminhnguyet205@gmail.com>
2025-11-01 17:16:09 +0700

    thông tin bác sĩ

---
+++ src/pages/info/PersonalDoctorPage.jsx
@@ -0,0 +1,10 @@
+import DoctorForm from '../../components/user/DoctorForm';
+function PersonalDoctorPage() {
+    return (
+        <>
+            <DoctorForm mode='detail' />
+        </>
+    );
+}
+
+export default PersonalDoctorPage;
