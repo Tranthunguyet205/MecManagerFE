d1a6c6cf93b3c4585122147279a014089cc04d9b
Tranthunguyet205 <157598331+Tranthunguyet205@users.noreply.github.com>
2025-11-20 13:15:07 +0700

    add ListDoctor

--- src/pages/list/ListDoctor.jsx
+++ src/pages/list/ListDoctor.jsx
@@ -1,8 +1,54 @@
+import { Table,Container, Pagination, Form, Button } from "react-bootstrap";
+import {useState} from "react"
 function ListDoctor() {
+    const [search, setSearch] = useState("")
+    const doctors = [
+        {name:"Nguyen Van A", CCHN: "CCHN/GPHN",sdt:"09029374",email:"exxample.@gamil.com",DVLV:"ha noi"},
+        {name:"Nguyen Van A", CCHN: "CCHN/GPHN",sdt:"09029374",email:"exxample.@gamil.com",DVLV:"ha noi"},
+        {name:"Nguyen Van A", CCHN: "CCHN/GPHN",sdt:"09029374",email:"exxample.@gamil.com",DVLV:"ha noi"}
+    ]
     return (
         <div>
-            <h1>List of Doctors</h1>
-            {/* Content for listing doctors will go here */}
+            <Container>
+                <Form className="">
+                    <div className="d-flex justify-between">
+                        <Form.Group type="text"
+                        placeholder="Tìm bác sĩ"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        className="input-field  w-75"
+                    />
+                    <Button className="bn-sm">Tìm kiem</Button>
+                    </div>
+                </Form>
+                <div className="table-responsive mt-3">
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Họ và tên</th>
+                                <th>CCHN/GPHN</th>
+                                <th>số điện thoại</th>
+                                <th>email</th>
+                                <th>Chi tiết</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {doctors.map((row,id) => (
+                                <tr key={id}>
+                                    <td>{row.name}</td>
+                                    <td>{row.CCHN}</td>
+                                    <td>{row.sdt}</td>
+                                    <td>{row.email}</td>
+                                    <td>
+                                        <button className="btn btn-outline-dark btn-sm">Xem chi tiết</button>
+                                    </td>
+                                    
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </Container>
         </div>
     );
 }
